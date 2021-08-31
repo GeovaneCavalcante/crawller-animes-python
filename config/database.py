4caf4bd2ef1ad33f55c80d08e766ca5cf869107a
GeovaneCavalcante <geovanefeitosacavalcante@gmail.com>
2021-08-31 09:44:53 -0300

    remove config database

--- config/database.py
+++ config/database.py
@@ -4,10 +4,10 @@ from pymongo import MongoClient
 
 class Database(object):
     def __init__(self):
-        # self._client = MongoClient(
-        #     'mongodb+srv://geovane:novembro123@cluster0.su9vu.mongodb.net')  # configure db url
         self._client = MongoClient(
-            'mongodb://localhost:27017/')  # configure db url
+            '')  # configure db url
+        # self._client = MongoClient(
+        #     'mongodb://localhost:27017/')  # configure db url
         self._db = self._client['animedb']
 
     def getDatabase(self):
